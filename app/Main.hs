cb994f5867a1fa4952713718e655eab24b964904
TomSmail <thomas.c.smail@gmail.com>
2022-12-02 14:26:46 +0000

    pretified code

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,24 @@
-module Main where
 import Data.List.Split
 import Data.List(sort)
 
 main :: IO ()
-main = do contents <- readFile "1.txt"
+main = day1
+
+day1:: IO ()
+day1 = do contents <- readFile "data/1.txt"
+          putStrLn "---------------"
+          putStrLn  "Day 1: \n "
           let stringArray = splitContent contents
           let intArray = map (map (\x -> read x :: Int) ) stringArray 
           let sumArray = map sum intArray
-          print "Solution 1"
+          putStrLn "\n Solution 1: "
           print $ foldl (\x y -> if x < y then y else x ) 0 sumArray
-          print "Solution 2"
+          putStrLn "\n Solution 2: "
           print $ sum $ take 3 $ reverse $ sort sumArray
+          putStrLn "---------------"
+
+          where splitContent:: String -> [[String]]
+                splitContent xs = splitWhen (== "") $ lines xs
 
 
-splitContent:: String -> [[String]]
-splitContent xs 
-    = splitWhen (== "") $ lines xs
 
--- splitWhen:: (a -> Bool) -> [a] -> [[a]]
--- splitWhen predicate xs = helper xs []
---     where helper [] holder = [holder]
---           helper (y:ys) holder 
---             | predicate y = helper ys (y : holder)
---             | otherwise = helper ys
